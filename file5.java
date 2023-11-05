df235fb5ae5b4a9fcc468c3934c555f77b05573c
ni <ni@gmail.com>
2023-11-05 15:34:41 +0000

    added f5 nd f6

---
+++ file5.java
@@ -0,0 +1,6 @@
+class file5 {
+public static void main(String[] args)
+{
+                System.out.println("hello");
+        }
+}
---
+++ file6.java
@@ -0,0 +1,6 @@
+class file6 {
+public static void main(String[] args)
+{
+                System.out.println("hello");
+        }
+}
